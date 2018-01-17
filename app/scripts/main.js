2b36b89e0247134d375456273ef7e37abb517e1f
farreldev <rickyalext@gmail.com>
2018-01-17 15:26:06 +0700

    perubahan headline artikel dan ekonografik

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -37,6 +37,45 @@
 					// settings: "unslick"
 					// instead of a settings object
 				]
+		})
+		
+		$('.ekonoGraphics').slick({
+         infinite: true,
+         slidesToShow: 4,
+         slidesToScroll: 1,
+         prevArrow: '.prevArrowNav',
+			nextArrow: '.nextArrowNav',
+         responsive: [
+            {
+					breakpoint: 1024,
+					settings: {
+							slidesToShow: 2,
+							slidesToScroll: 1,
+							infinite: true,
+							prevArrow: false,
+							nextArrow: false,
+						}
+					},{
+						breakpoint: 600,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1,
+							prevArrow: false,
+							nextArrow: false,
+						}
+					},{
+						breakpoint: 480,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1,
+							prevArrow: false,
+							nextArrow: false,
+						}
+					}
+					// You can unslick at a given breakpoint now by adding:
+					// settings: "unslick"
+					// instead of a settings object
+				]
       })
    }
 
