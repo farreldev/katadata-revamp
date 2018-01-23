f1847178fdd3910c2e3f16abfb5704370be5a8a3
farreldev <rickyalext@gmail.com>
2018-01-23 13:49:58 +0700

    save jan 23 2018

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,90 +1,103 @@
 (function($){
-   function kdSlider() {
-      $('.berita-slide').slick({
-         infinite: true,
-         slidesToShow: 4,
-         slidesToScroll: 1,
-         prevArrow: '.prevArrowNav',
-         nextArrow: '.nextArrowNav',
-         responsive: [
-            {
-					breakpoint: 1024,
-					settings: {
-							slidesToShow: 3,
-							slidesToScroll: 1,
-							infinite: true,
-							prevArrow: false,
-							nextArrow: false,
-						}
-					},{
-						breakpoint: 600,
+	var kd = function() {
+
+		function kdSlider() {
+			$('.berita-slide').slick({
+				infinite: true,
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				prevArrow: '.prevArrowNav',
+				nextArrow: '.nextArrowNav',
+				responsive: [
+					{
+						breakpoint: 1024,
 						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 1,
-							prevArrow: false,
-							nextArrow: false,
+								slidesToShow: 3,
+								slidesToScroll: 1,
+								infinite: true,
+								prevArrow: false,
+								nextArrow: false,
+							}
+						},{
+							breakpoint: 600,
+							settings: {
+								slidesToShow: 2,
+								slidesToScroll: 1,
+								prevArrow: false,
+								nextArrow: false,
+							}
+						},{
+							breakpoint: 480,
+							settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1,
+								prevArrow: false,
+								nextArrow: false,
+							}
 						}
-					},{
-						breakpoint: 480,
+					]
+			})
+			
+			$('.ekonoGraphics').slick({
+				infinite: true,
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				prevArrow: false,
+				nextArrow: false,
+				responsive: [
+					{
+						breakpoint: 1024,
 						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1,
-							prevArrow: false,
-							nextArrow: false,
+								slidesToShow: 2,
+								slidesToScroll: 1,
+								infinite: true,
+								prevArrow: false,
+								nextArrow: false,
+							}
+						},{
+							breakpoint: 600,
+							settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1,
+								prevArrow: false,
+								nextArrow: false,
+							}
+						},{
+							breakpoint: 480,
+							settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1,
+								prevArrow: false,
+								nextArrow: false,
+							}
 						}
-					}
-					// You can unslick at a given breakpoint now by adding:
-					// settings: "unslick"
-					// instead of a settings object
-				]
-		})
+						// You can unslick at a given breakpoint now by adding:
+						// settings: "unslick"
+						// instead of a settings object
+					]
+			})
+		}
+	
+		var ua = navigator.userAgent.toLowerCase(); 
+		var $tinggi = $('.footer').height();
 		
-		$('.ekonoGraphics').slick({
-         infinite: true,
-         slidesToShow: 4,
-         slidesToScroll: 1,
-         prevArrow: '.prevArrowNav',
-			nextArrow: '.nextArrowNav',
-         responsive: [
-            {
-					breakpoint: 1024,
-					settings: {
-							slidesToShow: 2,
-							slidesToScroll: 1,
-							infinite: true,
-							prevArrow: false,
-							nextArrow: false,
-						}
-					},{
-						breakpoint: 600,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1,
-							prevArrow: false,
-							nextArrow: false,
-						}
-					},{
-						breakpoint: 480,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1,
-							prevArrow: false,
-							nextArrow: false,
-						}
-					}
-					// You can unslick at a given breakpoint now by adding:
-					// settings: "unslick"
-					// instead of a settings object
-				]
-      })
-   }
+		$('.wrapper main').css('marginBottom', $tinggi)
+		
+		if (ua.indexOf('safari') != -1) { 
+			if (ua.indexOf('chrome') > -1) {
+				$('.wrapper main').css('marginBottom', $tinggi)
+			} else {
+				$('.wrapper main').css('marginBottom', $tinggi - 140)
+			}
+		}
+	
+		if(screen.width > 768) {
+			$('.matchingHeight').matchHeight();
+		}
 
-   var $tinggi = $('.footer').height();
-   $('.wrapper main').css('marginBottom', $tinggi)
+		$('#datetimepicker1').datetimepicker()
+	
+		kdSlider()
 
-	if(screen.width > 768) {
-		$('.matchingHeight').matchHeight();
-	}
-
-   kdSlider()
+	}()
 })(jQuery)
